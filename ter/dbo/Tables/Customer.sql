920e7ad75195490799c83c8ab62e2c0c9cab8ea3
pooja <pooja@DESKTOP-5B2816F>
2021-05-05 22:25:22 -0400

    hi i chedg

---
+++ ter/dbo/Tables/Customer.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Customer] (
+    [name]  NVARCHAR (MAX) NULL,
+    [id]    INT            NOT NULL,
+    [email] NCHAR (10)     NULL,
+    CONSTRAINT [PK_Customer] PRIMARY KEY CLUSTERED ([id] ASC)
+);
+
+
+
